27e9afd24986d0e65bba9ff9356a51f3a75aa947
lalatran <93048520+lalatran@users.noreply.github.com>
2021-11-04 23:42:26 -0400

    Added Lift and Tilt Sensor class.

---
+++ src/edu/fiu/tl/LiftTiltSensor.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package edu.fiu.tl;
+
+/**
+ * @author Tran
+ *
+ */
+public class LiftTiltSensor extends Sensor {
+
+	private int sensitivityLevel;
+	
+	public int getSlope() {
+		return 0;
+		
+	}
+}
